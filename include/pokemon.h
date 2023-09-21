0f09c1a25fc87b685adc9609f0a8da22a495f79d
opixelum <48217573+opixelum@users.noreply.github.com>
2023-09-21 23:33:20 +0200

    Add `pokemon.h`

---
+++ include/pokemon.h
@@ -0,0 +1,21 @@
+#ifndef POKEMON_H
+#define POKEMON_H
+
+#include "structs.h"
+
+/**
+ * @brief Prints a pokemon's stats
+ */
+void print_pokemon(Pokemon* pokemon);
+
+/**
+ * @brief Prints a team's pokemons
+ */
+void print_team(Team* team);
+
+/**
+ * @brief Prints all pokemons in a pokedex
+ */
+void print_pokedex(Pokedex* pokedex);
+
+#endif
