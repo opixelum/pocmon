f62d8eb91f1d19148a43a18fcb45ccfa1d3adffb
opixelum <48217573+opixelum@users.noreply.github.com>
2023-09-21 23:04:34 +0200

    Fix `structs.h` & add pokemon type enum

--- include/structs.h
+++ include/structs.h
@@ -1,13 +1,39 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum TYPE
+{
+    FIRE = 0,
+    WATER = 1,
+    PLANT = 2,
+    ELECTRIC = 3,
+    GROUND = 4,
+    NORMAL = 5,
+    PSY = 6,
+    FLYING = 7, 
+    STEEL = 8
+}
+TYPE;
+
+typedef enum TILE
+{
+    PLAYER = 0,
+    PATH = 1,
+    GRASS = 2,
+    obstacle = 3
+}
+TILE;
+
 typedef struct Pokemon {
     char* name;
-    int pv;
+    int hp;
     int attack;
     int defense;
     int speed;
-    int type;
+    TYPE type;
 } Pokemon;
 
 typedef struct Team {
@@ -29,10 +55,12 @@ typedef struct Pokedex {
 typedef struct Map {
     int columns;
     int lines;
-    enum tile {player, path, grass, obstacle};
+    TILE **tile;
 } Map;
 
 typedef struct Player {
     char* name;
     Team playerTeam;
 } Player;
+
+#endif
