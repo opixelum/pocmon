b0e08f3805b8af4a0243359df3424dd47b3b2916
Paul Gaudeaux <phvcgdx@gmail.com>
2023-09-22 00:32:10 +0200

    add event loop

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include "cli.h"
+#include "event_loop.h"
 
 int main(int argc, char **argv)
 {
-    main_menu();
+    if(event_loop() == 0) {
+        return EXIT_SUCCESS;
+    }
+    
     return EXIT_SUCCESS;
 }
