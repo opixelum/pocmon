62acbaabf8861dc41437051b0a0ee0810fb75afe
Opixelum <48217573+opixelum@users.noreply.github.com>
2023-09-22 02:45:17 +0200

    Add pokemon to pokedex (#24)
    * Add `add_pokemon_to_pokedex`
    
    * Rename hp parameter
    
    * Add `string.h` & `files.h` in `pokemon.h`
    
    * Add `get_pokemons_from_csv`
    
    * Fix loop index

--- include/pokemon.h
+++ include/pokemon.h
@@ -2,6 +2,8 @@
 #define POKEMON_H
 
 #include "structs.h"
+#include "files.h"
+#include "string.h"
 
 /**
  * @brief Prints a pokemon's stats
@@ -17,5 +19,27 @@ void print_team(Team* team);
  * @brief Prints all pokemons in a pokedex
  */
 void print_pokedex(Pokedex* pokedex);
+
+/**
+ * @brief Add a pokemon to a pokedex
+ * @param pokemon The pokemon to add
+ * @param pokedex The pokedex to add a pokemon to
+ * @return 0 if successful, 1 otherwise
+ */
+unsigned char add_pokemon_to_pokedex(Pokemon *pokemon, Pokedex *pokedex);
+
+/**
+ * @brief Get all pokemons listed in csv file
+ * @return An array of pokemons
+ * @warning Free the returned array after use
+ */
+Pokemon **get_pokemons_from_csv();
+
+/**
+ * @brief Convert pokemon type from string to number (unsigned char)
+ * @param string Type in string
+ * @return The corresponding type in integer
+ */
+unsigned char pokemon_type_string_to_num(char *string);
 
 #endif
