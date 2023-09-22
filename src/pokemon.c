7cbb316eec413b4e230125db2438232b22180196
opixelum <48217573+opixelum@users.noreply.github.com>
2023-09-22 02:41:08 +0200

    Add `get_pokemons_from_csv`

--- src/pokemon.c
+++ src/pokemon.c
@@ -43,3 +43,66 @@ unsigned char add_pokemon_to_pokedex(Pokemon *pokemon, Pokedex *pokedex)
     return EXIT_SUCCESS;
 } 
 
+unsigned char pokemon_type_string_to_num(char *string)
+{
+    if (strcmp(string, "Feu") == 0) return FIRE;
+    if (strcmp(string, "Eau") == 0) return WATER;
+    if (strcmp(string, "Plante") == 0) return PLANT;
+    if (strcmp(string, "Electrique") == 0) return ELECTRIC;
+    if (strcmp(string, "Sol") == 0) return GROUND;
+    if (strcmp(string, "Normal") == 0) return NORMAL;
+    if (strcmp(string, "Psy") == 0) return PSY;
+    if (strcmp(string, "Vol") == 0) return FLYING;
+    if (strcmp(string, "Acier") == 0) return STEEL;
+
+    return -1;
+}
+
+Pokemon **get_pokemons_from_csv()
+{
+    Csv *pokemon_csv = read_csv("./assets/pokemons.csv");
+    if (!pokemon_csv)
+    {
+        fprintf(stderr, "ERROR: get_pokemons_from_csv: pokemon_csv: read_csv failed.\n");
+        return NULL;
+    }
+
+    Pokemon **all_pokemons = malloc((pokemon_csv->nbRows - 1) * sizeof *all_pokemons);
+    if (!all_pokemons)
+    {
+        fprintf(stderr, "ERROR: get_pokemons_from_csv: all_pokemons: malloc failed.\n");
+    }
+
+    for (int i = 1; i < pokemon_csv->nbRows - 1; i++)
+    {
+        Pokemon *pokemon = malloc(sizeof *pokemon);
+        if (!pokemon)
+        {
+            fprintf(stderr, "ERROR: get_pokemons_from_csv: pokemon: malloc failed.\n");
+            return NULL;
+        }
+
+        pokemon->name = calloc
+        (
+            sizeof strlen(pokemon_csv->content[i][0] + 1),
+            sizeof pokemon->name
+        );
+        if (!pokemon->name)
+        {
+            fprintf(stderr, "ERROR: get_pokemons_from_csv: pokemon->name: calloc failed.\n");
+            return NULL;
+        }
+
+        strcpy(pokemon->name, pokemon_csv->content[i][0]);
+
+        pokemon->hp = atoi(pokemon_csv->content[i][1]);
+        pokemon->attack = atoi(pokemon_csv->content[i][2]);
+        pokemon->defense = atoi(pokemon_csv->content[i][3]);
+        pokemon->speed = atoi(pokemon_csv->content[i][4]);
+        pokemon->type = pokemon_type_string_to_num(pokemon_csv->content[i][5]);
+
+        all_pokemons[i - 1] = pokemon;
+    }
+
+    return all_pokemons;
+}
