572d27bda358b1611bc3eb7a42f35ae5b6875f39
opixelum <48217573+opixelum@users.noreply.github.com>
2023-09-22 02:38:55 +0200

    Add `string.h` & `files.h` in `pokemon.h`

--- include/pokemon.h
+++ include/pokemon.h
@@ -2,6 +2,8 @@
 #define POKEMON_H
 
 #include "structs.h"
+#include "files.h"
+#include "string.h"
 
 /**
  * @brief Prints a pokemon's stats
@@ -25,5 +27,19 @@ void print_pokedex(Pokedex* pokedex);
  * @return 0 if successful, 1 otherwise
  */
 unsigned char add_pokemon_to_pokedex(Pokemon *pokemon, Pokedex *pokedex);
+
+/**
+ * @brief Get all pokemons listed in csv file
+ * @return An array of pokemons
+ * @warning Free the returned array after use
+ */
+Pokemon **get_pokemons_from_csv();
+
+/**
+ * @brief Convert pokemon type from string to number (unsigned char)
+ * @param string Type in string
+ * @return The corresponding type in integer
+ */
+unsigned char pokemon_type_string_to_num(char *string);
 
 #endif
